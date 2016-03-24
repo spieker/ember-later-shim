726ffc215dd2061a9f665ea7d37d99aad7276151
Paul Spieker <paul.spieker@suitepad.de>
2016-03-24 10:30:33 +0100

    initial

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var defaults = require('lodash.defaults');
+var path = require('path');
+
 module.exports = {
-  name: 'ember-later-shim'
+  name: 'later',
+
+  included: function (app) {
+    this._super.included.apply(this, arguments);
+
+    this.app = app;
+    this.laterOptions = this.getConfig();
+
+    var vendor = this.treePaths.vendor;
+
+    app.import(vendor + '/later/later.min.js', { prepend: true });
+  },
+
+  treeForVendor: function (vendorTree) {
+    var trees = [];
+    var options = this.laterOptions;
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(new Funnel(options.laterPath, {
+      destDir: 'later',
+      include: [new RegExp(/\.js$/)],
+      exclude: ['test', 'benchmark', 'example', 'src'].map(function (key) {
+        return new RegExp(key + '\.js$');
+      }),
+    }));
+
+    return mergeTrees(trees);
+  },
+
+  getConfig: function () {
+    var projectConfig = ((this.project.config(process.env.EMBER_ENV) || {}).later || {});
+    var laterPath = path.dirname(require.resolve('later'));
+
+    var config = defaults(projectConfig, {
+      laterPath: laterPath,
+    });
+
+    return config;
+  },
 };
